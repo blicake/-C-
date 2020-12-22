c699aac24410078e503dbeb1dd8887404145c36f
blicake <katya.snisarenko@yandex.ru>
2020-12-22 15:35:50 +0500

    Swap

---
+++ Swap/Swap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Снисаренко Екатерина
+
+            // Написать программу обмена значениями двух переменных:
+            // а) с использованием третьей переменной;
+            // б) *без использования третьей переменной.
+
+            int a = 1;
+            int b = 2;
+            Console.WriteLine($"Переменная а = {a}, переменная b = {b}.");
+
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"Переменная а = {a}, переменная b = {b}.");
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"Переменная а = {a}, переменная b = {b}.");
+        }
+    }
+}
