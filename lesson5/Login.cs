47bb182bb253b517dea3076f6fd70a5bf7eb4051
blicake <katya.snisarenko@yandex.ru>
2021-01-21 19:56:13 +0300

    Login

---
+++ lesson5/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lesson5
+{
+    class Login
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите логин.");
+            string login = Console.ReadLine();
+
+            bool check = true;
+            if(login.Length < 2 || login.Length > 10) check = false;
+            if (Char.IsDigit(login, 0)) check = false;
+
+            for(int i = 0; i < login.Length; i++)
+            {
+                if (!Char.IsDigit(login, i) && (login[i] < 'a' || login[i] > 'z'))
+                {
+                    check= false;
+                    break;
+                }
+            }
+
+            if (check) Console.WriteLine("Принято");
+            else Console.WriteLine("Неправильный логин");
+        }
+    }
+}
